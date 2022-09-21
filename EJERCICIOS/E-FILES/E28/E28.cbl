814f1195cbfd81857fffe06f77b5dd5a8b079f9a
MiguelAngelMoyaJulio <mmoyajulio@frba.utn.edu.ar>
2022-09-20 23:16:00 -0300

    E28 modi

--- EJERCICIOS/E-FILES/E28/E28.cbl
+++ EJERCICIOS/E-FILES/E28/E28.cbl
@@ -15,26 +15,26 @@
        DATA DIVISION.
        FILE SECTION.
        FD DATOS1.
-           01 REG-DATOS1.
-               05 REG-SUCURSAL              PIC 9(02).
-               05 REG-VENDEDOR              PIC 9(02).
-               05 REG-FACTURA               PIC 9(02).
-               05 REG-MONTO                 PIC 9(07)V99.
+          01 REG-DATOS1.
+             05 REG-SUCURSAL              PIC 9(02).
+             05 REG-VENDEDOR              PIC 9(02).
+             05 REG-FACTURA               PIC 9(02).
+             05 REG-MONTO                 PIC 9(07)V99.
       ******************************************************************
       *                     WORKING-STORAGE SECTION   
-      ******************************************************************         
+      ******************************************************************
        WORKING-STORAGE SECTION.
-           01 FS-STATUS1                    PIC X(02) VALUE "00".
-               88 FS-STATUS-OK                        VALUE "00".
-               88 FS-STATUS-EOF                       VALUE "10".
-           01 WS-VAR.
-               02 WS-VENDEDOR-ANT          PIC 9(02).
-               02 WS-TOT-VENDEDOR          PIC 9(07)V99.
-               02 WS-SUCURSAL-ANT          PIC 9(02).
-               02 WS-TOT-SUC               PIC 9(07)V99.
-               02 WS-TOT-EMPRESA           PIC 9(07)V99.
-               02 WS-TOTE-MA               PIC ZZ,ZZZ,ZZZ.ZZ.
-           01 WS-TITULO.
+          01 FS-STATUS1                    PIC X(02) VALUE "00".
+             88 FS-STATUS-OK                        VALUE "00".
+             88 FS-STATUS-EOF                       VALUE "10".
+          01 WS-VAR.
+             02 WS-VENDEDOR-ANT          PIC 9(02).
+             02 WS-TOT-VENDEDOR          PIC 9(07)V99.
+             02 WS-SUCURSAL-ANT          PIC 9(02).
+             02 WS-TOT-SUC               PIC 9(07)V99.
+             02 WS-TOT-EMPRESA           PIC 9(07)V99.
+             02 WS-TOTE-MA               PIC ZZ,ZZZ,ZZZ.ZZ.
+          01 WS-TITULO.
                02 FILLER                   PIC X(03). 
                02 T-VENDEDOR               PIC 9(02). 
                02 FILLER                   PIC X(10). 
@@ -43,37 +43,36 @@
                02 T-MONTO                  PIC  ZZ,ZZZ,ZZZ.ZZ.
       ******************************************************************
       *                         PROCEDURE DIVISION   
-      ******************************************************************                           
-       PROCEDURE DIVISION.
-           
-           PERFORM 10-INICIO
-              THRU 10-INICIO-F
-           
-           PERFORM 20-PROCESO
-              THRU 20-PROCESO-F
-              UNTIL FS-STATUS-EOF           
-           
-           PERFORM 30-FIN
-              THRU 30-FIN-F
-           .
-            STOP RUN.
       ******************************************************************
-      *                         10-INICIO   
-      ******************************************************************      
-       10-INICIO.
-           
-           PERFORM 10-ABRIR-DATOS1
+       PROCEDURE DIVISION.
+                                                                          
+           PERFORM 10-INICIO                      
+              THRU 10-INICIO-F                    
+                                                  
+           PERFORM 20-PROCESO                     
+              THRU 20-PROCESO-F                   
+              UNTIL FS-STATUS-EOF                 
+                                                  
+           PERFORM 30-FIN                         
+              THRU 30-FIN-F                       
+           .                                      
+            STOP RUN.                             
+      ******************************************************************
+      *                         10-INICIO         
+      ******************************************************************
+       10-INICIO.                                 
+           PERFORM 10-ABRIR-DATOS1                
               THRU 10-ABRIR-DATOS1-F
-           
-           PERFORM 20-LEER1
-              THRU 20-LEER1-F
-           .
-       10-INICIO-F. EXIT.
+                            
+           PERFORM 20-LEER1                       
+              THRU 20-LEER1-F                     
+           .                                      
+       10-INICIO-F. EXIT.                         
       ******************************************************************
       *                         10-ABRIR-DATOS1   
-      ******************************************************************     
-       10-ABRIR-DATOS1.
-           OPEN INPUT DATOS1
+      ******************************************************************
+       10-ABRIR-DATOS1.                        
+           OPEN INPUT DATOS1                   
            IF NOT FS-STATUS-OK
                DISPLAY "ERROR AL ABRIR ARCHIVO " FS-STATUS1
            END-IF
@@ -82,7 +81,8 @@
       ******************************************************************
       *                         20-PROCESO   
       ****************************************************************** 
-       20-PROCESO.  
+       20-PROCESO.
+             
            MOVE REG-SUCURSAL TO WS-SUCURSAL-ANT
            INITIALIZE WS-TOT-SUC    
            
