b7e8d83c28689daf4c3bd98250adf3e9b6f870bb
MiguelAngelMoyaJulio <mmoyajulio@frba.utn.edu.ar>
2022-10-09 20:27:09 -0300

    E2 DIVISION BY ZERO REVIEW

--- EJERCICIOS/E2.cbl
+++ EJERCICIOS/E2.cbl
@@ -1,39 +1,80 @@
       ******************************************************************
-      * Author: MIGUEL MOYA
-      * Date: 2022-09-06
       * Dados dos valores numéricos enteros a y b calcular e informar 
       * el cosiente a/b. Considere que b puede ser cero. 
       * En ese caso mostrar el correspondiente mensaje de error.
       ******************************************************************
        IDENTIFICATION DIVISION.
        PROGRAM-ID. E2.
+       AUTHOR. MIGUEL MOYA.
+       DATE-WRITTEN. OCTOBER 2022.
+       DATE-COMPILED. OCTOBER 2022.
+      ******************************************************************
+      *                     ENVIRONMENT DIVISION
+      ****************************************************************** 
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       SPECIAL-NAMES.
+             DECIMAL-POINT IS COMMA.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+      *****************************  INPUT  ****************************
+       
+      ****************************  OUTPUT  **************************** 
        DATA DIVISION.
        FILE SECTION.
+      ******************************************************************
+      *                     WORKING-STORAGE SECTION   
+      ******************************************************************
        WORKING-STORAGE SECTION.
-       77 WS-A PIC 9(03).
-       77 WS-B PIC 9(03).
-       77 WS-DIV PIC 9(04).
-       77 WS-REMAINDER PIC 9(04).
+      ************************  CONSTANTES  ****************************
+       01 WS-CON.       
+      **************************  SWITCHES  ****************************
+       01 WS-SWITCHES.       
+      ************************** VARIABLES *****************************
+       01 WS-VAR.
+          02 WS-A PIC 9(03).
+          02 WS-B PIC 9(03).
+          02 WS-DIV PIC 9(04).
+          02 WS-REMAINDER PIC 9(04).
+       LINKAGE SECTION.        
+      ******************************************************************
+      *                         PROCEDURE DIVISION   
+      ******************************************************************
        PROCEDURE DIVISION.
-            PERFORM 100-LOAD
-               THRU 100-LOAD-F
+           PERFORM 100000-START
+              THRU 100000-START-F
            
-            PERFORM 200-PROCESS
-               THRU 200-PROCESS-F
+           PERFORM 200000-PROCESS
+              THRU 200000-PROCESS-F
+           
+           PERFORM 300000-END
+              THRU 300000-END-F
            .
-            STOP RUN.
-       100-LOAD.
+      ******************************************************************
+      *                         100000-START         
+      ******************************************************************      
+       100000-START.
            DISPLAY "ENTER THE FIRST NUMBER "
            ACCEPT WS-A
            DISPLAY "ENTER THE SECOND NUMBER "
            ACCEPT WS-B
-               .
-       100-LOAD-F.
-
-       200-PROCESS.
+           .
+       100000-START-F. EXIT.
+      ******************************************************************
+      *                         200000-PROCESS         
+      ******************************************************************
+       200000-PROCESS.
            DIVIDE WS-A BY WS-B GIVING WS-DIV REMAINDER WS-REMAINDER
+           .
+       200000-PROCESS-F. EXIT.
+      ******************************************************************
+      *                         300000-END   
+      ****************************************************************** 
+       300000-END.
            DISPLAY "DIVISION " WS-A " / " WS-B " : " WS-DIV
            DISPLAY "REMAINDER " WS-REMAINDER
-           .
-       200-PROCESS-F.
+           STOP RUN 
+           .    
+       300000-END-F. EXIT. 
+       
        END PROGRAM E2.
