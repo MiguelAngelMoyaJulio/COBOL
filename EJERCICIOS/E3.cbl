e263b6460de7889c417996709ad49759e6f7c8ca
MiguelAngelMoyaJulio <mmoyajulio@frba.utn.edu.ar>
2022-10-09 20:34:23 -0300

    E3 DIVISION AND RAMAINDER REVIEW

--- EJERCICIOS/E3.cbl
+++ EJERCICIOS/E3.cbl
@@ -1,6 +1,4 @@
       ******************************************************************
-      * Author: MIGUEL MOYA
-      * Date: 2022-09-06
       * Dado valor numérico entero que se ingresa por teclado, 
       * se pide informar:
       * •	La quinta parte de dicho valor,
@@ -9,37 +7,81 @@
       ******************************************************************
        IDENTIFICATION DIVISION.
        PROGRAM-ID. E3.
+       AUTHOR. MIGUEL MOYA.
+       DATE-WRITTEN. OCTOBER 2022.
+       DATE-COMPILED. OCTOBER 2022.
+      ******************************************************************
+      *                     ENVIRONMENT DIVISION
+      ****************************************************************** 
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       SPECIAL-NAMES.
+             DECIMAL-POINT IS COMMA.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+      *****************************  INPUT  ****************************
+       
+      ****************************  OUTPUT  ****************************
+
        DATA DIVISION.
        FILE SECTION.
+      ******************************************************************
+      *                     WORKING-STORAGE SECTION   
+      ******************************************************************
        WORKING-STORAGE SECTION.
-       77 WS-A PIC 9(03).
-       77 WS-B PIC 9(03).
-       77 WS-Q PIC 9(04).
-       77 WS-DIV PIC 9(04).
-       77 WS-BY-FIVE PIC 9(04).
-       77 WS-S PIC 9(04)V9(02).
-       77 WS-REMAINDER PIC 9(04).
+      ************************  CONSTANTES  ****************************
+       01 WS-CON.       
+      **************************  SWITCHES  ****************************
+       01 WS-SWITCHES.       
+      ************************** VARIABLES *****************************
+       01 WS-VAR.
+          02 WS-A         PIC 9(03).
+          02 WS-B         PIC 9(03).
+          02 WS-Q         PIC 9(04).
+          02 WS-DIV       PIC 9(04).
+          02 WS-BY-FIVE   PIC 9(04).
+          02 WS-S         PIC 9(04)V9(02).
+          02 WS-REMAINDER PIC 9(04).
+       LINKAGE SECTION.        
+      ******************************************************************
+      *                         PROCEDURE DIVISION   
+      ****************************************************************** 
        PROCEDURE DIVISION.
-           PERFORM 100-LOAD
-              THRU 100-LOAD-F
-           
-           PERFORM 200-PROCESS
-              THRU 200-PROCESS-F
+           PERFORM 100000-START                      
+              THRU 100000-START-F                    
+                                                  
+           PERFORM 200000-PROCESS                     
+              THRU 200000-PROCESS-F                   
+                                                  
+           PERFORM 300000-END                         
+              THRU 300000-END-F 
            .
-           STOP RUN.
-       100-LOAD.
+      ******************************************************************
+      *                         100000-START   
+      ******************************************************************
+       100000-START.
            DISPLAY "ENTER THE FIRST NUMBER "
            ACCEPT WS-A
            .
-       100-LOAD-F.
-
-       200-PROCESS.
+       100000-START-F.
+      ******************************************************************
+      *                         200000-PROCESS   
+      ******************************************************************
+       200000-PROCESS.
            DIVIDE WS-A BY 5 GIVING WS-DIV REMAINDER WS-REMAINDER
            DIVIDE WS-DIV BY 7 GIVING WS-S
            COMPUTE WS-BY-FIVE = WS-REMAINDER * 5
+           .
+       200000-PROCESS-F. EXIT.
+      ******************************************************************
+      *                         300000-END   
+      ****************************************************************** 
+       300000-END.
            DISPLAY "DIVISION BY FIVE " WS-A " / 5 : " WS-DIV
            DISPLAY "RAMAINDER BY FIVE " WS-A " * 5 : " WS-BY-FIVE
            DISPLAY "DIVIDING THE FIRST DIVISION BY SEVEN " WS-S 
-           .
-       200-PROCESS-F.
+           STOP RUN 
+           .    
+       300000-END-F. EXIT.
+
        END PROGRAM E3.
