bab6686ad69b538beea50bcc426705ce41294732
MiguelAngelMoyaJulio <mmoyajulio@frba.utn.edu.ar>
2022-10-09 20:37:00 -0300

    E3 DIVISION AND RAMAINDER REVIEW

--- EJERCICIOS/E4.cbl
+++ EJERCICIOS/E4.cbl
@@ -1,25 +1,55 @@
       ******************************************************************
-      * Author: MIGUEL MOYA
-      * Date: 2022-09-06
       * Dados dos valores numéricos, informar cual es el mayor y 
       * cual es el menor o, si ambos valores 
       * son iguales emitir un mensaje
       ******************************************************************
        IDENTIFICATION DIVISION.
        PROGRAM-ID. E4.
+       AUTHOR. MIGUEL MOYA.
+       DATE-WRITTEN. OCTOBER 2022.
+       DATE-COMPILED. OCTOBER 2022.
+      ******************************************************************
+      *                     ENVIRONMENT DIVISION
+      ****************************************************************** 
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       SPECIAL-NAMES.
+             DECIMAL-POINT IS COMMA.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+      *****************************  INPUT  ****************************
+       
+      ****************************  OUTPUT  ****************************
+
        DATA DIVISION.
        FILE SECTION.
+      ******************************************************************
+      *                     WORKING-STORAGE SECTION   
+      ******************************************************************
        WORKING-STORAGE SECTION.
-       77 WS-A PIC 9(03).
-       77 WS-B PIC 9(03).
+      ************************  CONSTANTES  ****************************
+       01 WS-CON.       
+      **************************  SWITCHES  ****************************
+       01 WS-SWITCHES.       
+      ************************** VARIABLES *****************************
+       01 WS-VAR.
+          02 WS-A PIC 9(03).
+          02 WS-B PIC 9(03).
+       LINKAGE SECTION.         
+       ******************************************************************
+      *                         PROCEDURE DIVISION   
+      ******************************************************************
        PROCEDURE DIVISION.
-           PERFORM 100-LOAD
-              THRU 100-LOAD-F
-           
-           PERFORM 200-PROCESS
-              THRU 200-PROCESS-F
-           .
-           STOP RUN.
+           PERFORM 100000-START                      
+              THRU 100000-START-F                    
+                                                  
+           PERFORM 200000-PROCESS                     
+              THRU 200000-PROCESS-F                   
+              UNTIL CONDICION                 
+                                                  
+           PERFORM 300000-END                         
+              THRU 300000-END-F   
+           . 
        100-LOAD.
            DISPLAY "ENTER THE FIRST NUMBER "
            ACCEPT WS-A
@@ -39,5 +69,13 @@
                END-IF 
            END-IF
            .
-       200-PROCESS-F.
+       200-PROCESS-F. EXIT.
+      ******************************************************************
+      *                         300000-END   
+      ****************************************************************** 
+       300000-END.
+           DISPLAY "FIN"
+           STOP RUN 
+           .    
+       300000-END-F. EXIT. 
        END PROGRAM E4.
