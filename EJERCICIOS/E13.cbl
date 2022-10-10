cb6c23157c6573ef6582aa2a361c0bf7583f981b
MiguelAngelMoyaJulio <mmoyajulio@frba.utn.edu.ar>
2022-10-09 23:51:41 -0300

    E13 MOD 3 - NOT MOD 5 REVIEW

--- EJERCICIOS/E13.cbl
+++ EJERCICIOS/E13.cbl
@@ -1,15 +1,37 @@
       ******************************************************************
-      * Author: MIGUEL MOYA
-      * Date: 2022-09-07
       * Dado un valor numérico entero m, determinar e imprimir un 
       * listado con los m primeros 
       * múltiplos de 3 que no sean múltiples de 5
       ******************************************************************
        IDENTIFICATION DIVISION.
        PROGRAM-ID. E13.
+       AUTHOR. MIGUEL MOYA.
+       DATE-WRITTEN. OCTOBER 2022.
+       DATE-COMPILED. OCTOBER 2022.
+      ******************************************************************
+      *                     ENVIRONMENT DIVISION
+      ****************************************************************** 
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       SPECIAL-NAMES.
+             DECIMAL-POINT IS COMMA.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+      *****************************  INPUT  ****************************
+       
+      ****************************  OUTPUT  ****************************
+
        DATA DIVISION.
        FILE SECTION.
+      ******************************************************************
+      *                     WORKING-STORAGE SECTION   
+      ******************************************************************
        WORKING-STORAGE SECTION.
+      ************************  CONSTANTES  ****************************
+
+      **************************  SWITCHES  ****************************
+
+      ************************** VARIABLES *****************************
        01 WS-VAR.
           02 WS-NUM        PIC 9(03). 
           02 WS-I          PIC 9(03). 
@@ -18,27 +40,36 @@
           02 WS-RES-3      PIC 9(03). 
           02 WS-RES-5      PIC 9(03). 
           02 WS-MOD-5      PIC 9(03). 
+      ******************************************************************
+      *                       LINKAGE SECTION   
+      ****************************************************************** 
+       LINKAGE SECTION.        
+      ******************************************************************
+      *                         PROCEDURE DIVISION   
+      ****************************************************************** 
        PROCEDURE DIVISION.
-           PERFORM 10-LOAD
-              THRU 10-LOAD-F
-           
-           PERFORM 20-PROCESS
-              THRU 20-PROCESS-F
-           .
-           STOP RUN.
-       10-LOAD.
+           PERFORM 100000-START                      
+              THRU 100000-START-F                    
+                                                  
+           PERFORM 200000-PROCESS                     
+              THRU 200000-PROCESS-F    
+              UNTIL WS-I = WS-NUM               
+                                                  
+           PERFORM 300000-END                         
+              THRU 300000-END-F
+           .   
+      ******************************************************************
+      *                         100000-START         
+      ******************************************************************     
+       100000-START.
            DISPLAY "ENTER A NUMBER "
            ACCEPT WS-NUM
            .
-       10-LOAD-F.
-
-       20-PROCESS.
-           PERFORM 30-CALCULO
-           THRU 30-CALCULO-F 
-           UNTIL WS-I = WS-NUM
-           .
-       20-PROCESS-F.
-       30-CALCULO.
+       100000-START-F. EXIT.
+      ******************************************************************
+      *                         200000-PROCESS         
+      ******************************************************************
+       200000-PROCESS.
            ADD 1 TO WS-X
            DIVIDE WS-X BY 3 GIVING WS-RES-3 REMAINDER WS-MOD-3
            DIVIDE WS-X BY 5 GIVING WS-RES-5 REMAINDER WS-MOD-5
@@ -48,6 +79,14 @@
                ADD 1 TO WS-I
            END-IF
            .
-       30-CALCULO-F.
-
+       200000-PROCESS-F. EXIT.
+      ******************************************************************
+      *                         300000-END   
+      ****************************************************************** 
+       300000-END.
+           DISPLAY "FIN"
+           STOP RUN 
+           .    
+       300000-END-F. EXIT.
+       
        END PROGRAM E13.
