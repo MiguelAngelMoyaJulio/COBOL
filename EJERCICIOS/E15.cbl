6ede2cd7592d263ff454aad40b7bffce5fa1eef5
MiguelAngelMoyaJulio <mmoyajulio@frba.utn.edu.ar>
2022-10-10 00:35:24 -0300

    E15 STRINGS REVIEW

--- EJERCICIOS/E15.cbl
+++ EJERCICIOS/E15.cbl
@@ -1,6 +1,4 @@
       ******************************************************************
-      * Author: MIGUEL MOYA
-      * Date: 2022-09-06
       * Dada una serie de caracteres que conforman una oración
       * , donde cada palabra está separada de la 
       * siguiente por un carácter blanco y 
@@ -12,9 +10,35 @@
       ******************************************************************
        IDENTIFICATION DIVISION.
        PROGRAM-ID. E15.
+       AUTHOR. MIGUEL MOYA.
+       DATE-WRITTEN. OCTOBER 2022.
+       DATE-COMPILED. OCTOBER 2022.
+      ******************************************************************
+      *                     ENVIRONMENT DIVISION
+      ****************************************************************** 
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       SPECIAL-NAMES.
+             DECIMAL-POINT IS COMMA.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+      *****************************  INPUT  ****************************
+       
+      ****************************  OUTPUT  ****************************
+ 
        DATA DIVISION.
        FILE SECTION.
+      ******************************************************************
+      *                     WORKING-STORAGE SECTION   
+      ******************************************************************
        WORKING-STORAGE SECTION.
+      ************************  CONSTANTES  ****************************
+       01 WS-CONS.
+           02 CON-BLANK             PIC X(01) VALUE " ".     
+           02 CON-POINT             PIC X(01) VALUE ".".     
+      **************************  SWITCHES  ****************************
+
+      ************************** VARIABLES *****************************
        01 WS-VAR. 
            02 WS-WORD               PIC X(250) VALUE SPACES. 
            02 WS-A                  PIC 9(04)  VALUE ZEROS. 
@@ -34,34 +58,43 @@
            02 WS-POS-3              PIC 9(03)  VALUE ZEROS.
            02 WS-MAX-LEN-WORD       PIC 9(03)  VALUE ZEROS.
            02 WS-PARTIAL-LEN-WORD   PIC 9(03)  VALUE ZEROS.
-
-       01 CONS.
-           02 CON-BLANK             PIC X(01) VALUE " ".     
-           02 CON-POINT             PIC X(01) VALUE ".".     
+      ******************************************************************
+      *                       LINKAGE SECTION   
+      ****************************************************************** 
+       LINKAGE SECTION.        
+      ******************************************************************
+      *                         PROCEDURE DIVISION   
+      ****************************************************************** 
        PROCEDURE DIVISION.
-           PERFORM 100-LOAD
-              THRU 100-LOAD-F
-           IF WS-AMOUNT-SPACES NOT EQUAL 250
-               PERFORM 200-PROCESS
-                  THRU 200-PROCESS-F
-           END-IF   
+           PERFORM 100000-START                      
+              THRU 100000-START-F                    
+                                                  
+           PERFORM 200000-PROCESS                     
+              THRU 200000-PROCESS-F
+             UNTIL WS-POINT-FOUND = "Y"   
+                                                  
+           PERFORM 300000-END                         
+              THRU 300000-END-F
            .
-           STOP RUN.
-       100-LOAD.
+      ******************************************************************
+      *                         100000-START         
+      ******************************************************************     
+       100000-START.
            DISPLAY "ENTER A SENTENCE "
            ACCEPT WS-WORD       
            
            PERFORM VARYING WS-J FROM 1 BY 1
            UNTIL WS-J > 250
-               IF WS-WORD(WS-J:1) = CON-BLANK
+               IF WS-WORD(WS-J:1) EQUAL SPACES
                    ADD 1 TO WS-AMOUNT-SPACES
                END-IF
            END-PERFORM           
            . 
-       100-LOAD-F.
-
-       200-PROCESS.
-
+       100000-START-F. EXIT.
+      ******************************************************************
+      *                         200000-PROCESS         
+      ******************************************************************
+       200000-PROCESS.
            MOVE "N" TO WS-POINT-FOUND
            PERFORM VARYING WS-INDEX FROM 1
            BY 1 UNTIL WS-POINT-FOUND = "Y"
@@ -85,15 +118,15 @@
                    END-IF
                    
                    EVALUATE TRUE
-                       WHEN WS-WORD(WS-INDEX:1) = "a" 
+                       WHEN WS-WORD(WS-INDEX:1) = "A" 
                             ADD 1 TO WS-A 
-                       WHEN WS-WORD(WS-INDEX:1) = "e"
+                       WHEN WS-WORD(WS-INDEX:1) = "E"
                             ADD 1 TO WS-E 
-                       WHEN WS-WORD(WS-INDEX:1) = "i"
+                       WHEN WS-WORD(WS-INDEX:1) = "I"
                             ADD 1 TO WS-I 
-                       WHEN WS-WORD(WS-INDEX:1) = "o"
+                       WHEN WS-WORD(WS-INDEX:1) = "O"
                             ADD 1 TO WS-O 
-                       WHEN WS-WORD(WS-INDEX:1) = "u"
+                       WHEN WS-WORD(WS-INDEX:1) = "U"
                             ADD 1 TO WS-U 
                    END-EVALUATE
                END-IF
@@ -107,14 +140,36 @@
 
            COMPUTE WS-CANT-SPACES= WS-CANT-SPACES + 1
 
-           DISPLAY "TOTALS "
-           DISPLAY "VOWEL A " WS-A 
-           DISPLAY "VOWEL E " WS-E 
-           DISPLAY "VOWEL I " WS-I 
-           DISPLAY "VOWEL O " WS-O 
-           DISPLAY "VOWEL U " WS-U 
-           DISPLAY "TOTAL WORDS " WS-CANT-SPACES 
-           DISPLAY "LONGEST WORD " WS-MAX-LEN-WORD 
            .
-       200-PROCESS-F.
+       200000-PROCESS-F. EXIT.
+      ******************************************************************
+      *                         300000-END   
+      ****************************************************************** 
+       300000-END.
+           PERFORM 310000-TOTAL
+              THRU 310000-TOTAL-F
+
+           STOP RUN 
+           .    
+       300000-END-F. EXIT. 
+      ******************************************************************
+      *                         310000-TOTAL   
+      ****************************************************************** 
+       310000-TOTAL.
+           IF WS-AMOUNT-SPACES <> 250
+               DISPLAY "TOTALS "
+               DISPLAY "VOWEL A " WS-A 
+               DISPLAY "VOWEL E " WS-E 
+               DISPLAY "VOWEL I " WS-I 
+               DISPLAY "VOWEL O " WS-O 
+               DISPLAY "VOWEL U " WS-U 
+               DISPLAY "TOTAL WORDS " WS-CANT-SPACES 
+               DISPLAY "LONGEST WORD " WS-MAX-LEN-WORD 
+           ELSE
+               DISPLAY "NO DATA TO BE PROCESSED"
+           END-IF
+           DISPLAY "END"
+           .    
+       310000-TOTAL-F. EXIT. 
+
        END PROGRAM E15.
