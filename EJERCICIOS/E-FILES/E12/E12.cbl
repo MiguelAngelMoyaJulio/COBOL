994a82e95b56ae4d33d13fd57873f46237612d81
MiguelAngelMoyaJulio <mmoyajulio@frba.utn.edu.ar>
2022-10-09 23:42:01 -0300

    E12 FILE - MAX AND MIN REVIEW

--- EJERCICIOS/E-FILES/E12/E12.cbl
+++ EJERCICIOS/E-FILES/E12/E12.cbl
@@ -28,67 +28,131 @@
        FD DATOS.
           01 REG-DATOS.
              05 REG-NUM PIC 9(03).
+      ******************************************************************
+      *                     WORKING-STORAGE SECTION   
+      ******************************************************************
        WORKING-STORAGE SECTION.
+      ************************  CONSTANTES  ****************************
+
+      **************************  SWITCHES  ****************************
+       01 WS-SWITCHES.       
+          05 FS-STATUS               PIC X(02) VALUE "00".
+             88 FS-STATUS-OK                   VALUE "00".
+             88 FS-STATUS-EOF                  VALUE "10".
+      ************************** VARIABLES *****************************
        01 WS-VAR.
           02 WS-MIN         PIC 9(03). 
           02 WS-MAX         PIC 9(03). 
           02 WS-POS-MAX     PIC 9(03). 
           02 WS-POS-MIN     PIC 9(03).
           02 WS-POS         PIC 9(03).
-       77 WS-STATUS PIC X(01).
-
-           
+      ******************************************************************
+      *                       LINKAGE SECTION   
+      ****************************************************************** 
+       LINKAGE SECTION.        
+      ******************************************************************
+      *                         PROCEDURE DIVISION   
+      ****************************************************************** 
        PROCEDURE DIVISION.
-       OPEN INPUT DATOS
-       
-       PERFORM 20-LEER
-       THRU 20-LEER-F
-       
-       MOVE REG-NUM   TO WS-MAX
-       MOVE REG-NUM   TO WS-MIN
+           PERFORM 100000-START                      
+              THRU 100000-START-F                    
+                                                  
+           PERFORM 200000-PROCESS                     
+              THRU 200000-PROCESS-F 
+              UNTIL FS-STATUS-EOF                   
+                                                  
+           PERFORM 300000-END                         
+              THRU 300000-END-F
+           .
+      ******************************************************************
+      *                         100000-START         
+      ******************************************************************
+       100000-START.            
+           PERFORM 110000-OPEN-DATOS
+              THRU 110000-OPEN-DATOS-F
+              
+           PERFORM 210000-READ-DATOS
+              THRU 210000-READ-DATOS-F
 
-       PERFORM 30-CALCULO
-       THRU 30-CALCULO-F
-       UNTIL WS-STATUS = "F"
-       
-       CLOSE DATOS
+           MOVE REG-NUM   TO WS-MAX
+           MOVE REG-NUM   TO WS-MIN                        
+           .                                      
+       100000-START-F. EXIT.
+      ******************************************************************
+      *                         110000-OPEN-DATOS   
+      ******************************************************************
+       110000-OPEN-DATOS.                        
+           OPEN INPUT DATOS                   
+           IF NOT FS-STATUS-OK
+               DISPLAY "ERROR AL ABRIR ARCHIVO : " FS-STATUS
+           END-IF
+           .
+       110000-OPEN-DATOS-F. EXIT.
+      ******************************************************************
+      *                         200000-PROCESS   
+      ****************************************************************** 
+       200000-PROCESS.
+           IF REG-NUM > WS-MAX
+              MOVE REG-NUM TO WS-MAX
+              MOVE WS-POS  TO WS-POS-MAX 
+           END-IF
 
-       PERFORM 40-TOTALES
-          THRU 40-TOTALES-F
-       . 
-       STOP RUN.
+           IF REG-NUM < WS-MIN
+              MOVE REG-NUM TO WS-MIN 
+              MOVE WS-POS  TO WS-POS-MIN 
+           END-IF
+           
+           PERFORM 210000-READ-DATOS
+              THRU 210000-READ-DATOS-F
+           .         
+       200000-PROCESS-F. EXIT.
+      ******************************************************************
+      *                         210000-READ-DATOS   
+      ******************************************************************      
+       210000-READ-DATOS.
+           INITIALIZE REG-DATOS
+           READ DATOS INTO REG-DATOS
+           EVALUATE TRUE
+               WHEN FS-STATUS-OK
+                    ADD 1 TO WS-POS   
+               WHEN FS-STATUS-EOF
+                    CONTINUE
+           END-EVALUATE
+           .
+       210000-READ-DATOS-F. EXIT.
+      ******************************************************************
+      *                         300000-END   
+      ****************************************************************** 
+       300000-END.
 
-       20-LEER. 
-       ADD 1 TO WS-POS
+           PERFORM 310000-CLOSE-DATOS
+              THRU 310000-CLOSE-DATOS-F
+           
+           PERFORM 320000-TOTAL
+              THRU 320000-TOTAL-F
 
-       READ DATOS NEXT RECORD
-       AT END
-       MOVE "F" TO  WS-STATUS
-       .            
-       20-LEER-F.
-
-       30-CALCULO. 
-       
-       IF REG-NUM > WS-MAX
-          MOVE REG-NUM TO WS-MAX
-          MOVE WS-POS TO WS-POS-MAX 
-       END-IF
-
-       IF REG-NUM < WS-MIN
-          MOVE REG-NUM TO WS-MIN 
-          MOVE WS-POS TO WS-POS-MIN 
-       END-IF
-
-       PERFORM 20-LEER
-       THRU 20-LEER-F
-       .
-       30-CALCULO-F.  
-
-       40-TOTALES. 
+           STOP RUN 
+           .    
+       300000-END-F. EXIT. 
+      ******************************************************************
+      *                         310000-CLOSE-DATOS   
+      ****************************************************************** 
+       310000-CLOSE-DATOS.
+           CLOSE DATOS
+           IF NOT FS-STATUS-OK
+               DISPLAY "ERROR AL CERRAR ARCHIVO DATOS " FS-STATUS
+           END-IF
+           .
+       310000-CLOSE-DATOS-F. EXIT.  
+      ******************************************************************
+      *                         320000-TOTAL   
+      ****************************************************************** 
+       320000-TOTAL.
            DISPLAY "MAX NUMBER " WS-MAX           
                    "  POSITION " WS-POS-MAX           
            DISPLAY "MIN NUMBER " WS-MIN           
                    "  POSITION " WS-POS-MIN           
-           .           
-       40-TOTALES-F.            
+           .
+       320000-TOTAL-F. EXIT.               
+
        END PROGRAM E12.
