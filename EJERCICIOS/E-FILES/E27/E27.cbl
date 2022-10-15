d55a0e0248b98493f39468aee2b55be67fe62016
MiguelAngelMoyaJulio <mmoyajulio@frba.utn.edu.ar>
2022-10-15 20:11:55 -0300

    E27 CONTROL CUT - SUCURSAL

--- EJERCICIOS/E-FILES/E27/E27.cbl
+++ EJERCICIOS/E-FILES/E27/E27.cbl
@@ -5,13 +5,24 @@
       ******************************************************************
        IDENTIFICATION DIVISION.
        PROGRAM-ID. E27.
+       AUTHOR. MIGUEL MOYA.
+       DATE-WRITTEN. OCTOBER 2022.
+       DATE-COMPILED. OCTOBER 2022.
+      ******************************************************************
+      *                     ENVIRONMENT DIVISION
+      ******************************************************************
        ENVIRONMENT DIVISION.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
-       SELECT OPTIONAL DATOS1
-       ASSIGN TO "SUC-VEN.txt"
-       FILE STATUS IS FS-STATUS1
-       ORGANIZATION IS LINE SEQUENTIAL.
+      ******************************************************************
+      *                            FILES   
+      ******************************************************************
+      *****************************  INPUT  **************************** 
+       SELECT DATOS1 ASSIGN TO "SUC-VEN.txt"
+                     FILE STATUS IS FS-STATUS-FILE
+                     ORGANIZATION IS LINE SEQUENTIAL.
+      ****************************  OUTPUT  **************************** 
+
        DATA DIVISION.
        FILE SECTION.
        FD DATOS1.
@@ -23,142 +34,131 @@
       *                     WORKING-STORAGE SECTION   
       ******************************************************************         
        WORKING-STORAGE SECTION.
-           01 FS-STATUS1                    PIC X(02) VALUE "00".
-               88 FS-STATUS-OK                        VALUE "00".
-               88 FS-STATUS-EOF                       VALUE "10".
-           01 WS-VAR.
-               02 WS-VENDEDOR-ANT          PIC 9(02).
-               02 WS-TOT-VENDEDOR          PIC 9(07)V99.
-               02 WS-SUCURSAL-ANT          PIC 9(02).
-               02 WS-TOT-SUC               PIC 9(07)V99.
-               02 WS-TOT-EMPRESA           PIC 9(07)V99.
-               02 WS-TOTE-MA               PIC ZZ,ZZZ,ZZZ.ZZ.
-           01 WS-TITULO.
-               02 WS-T1                    PIC  X(04) VALUE "V1 [".                 
-               02 WS-I1                    PIC  9(02).                  
-               02 WS-F1                    PIC  X(05) VALUE "] -> ".                 
-               02 WS-VALOR1                PIC  X(02).                 
-               02 WS-T2                    PIC  X(07) VALUE " + V2 [".                 
-               02 WS-I2                    PIC  9(02).                  
-               02 WS-F2                    PIC  X(05) VALUE "] -> ".                 
-               02 WS-VALOR2                PIC  9(02).                 
-               02 WS-OP                    PIC  X(03) VALUE " = ".                 
-               02 WS-T-SUMA                PIC  9(03).
+      ************************  CONSTANTS  *****************************
+
+      ************************** TABLES ********************************
+
+      **************************  SWITCHES  **************************** 
+       01 FS-STATUS-FILE               PIC X(02) VALUE "00".
+           88 FS-STATUS-FILE-OK                  VALUE "00".
+           88 FS-STATUS-FILE-EOF                 VALUE "10".
+
+      ************************** VARIABLES *****************************
+       01 WS-VAR.
+           02 WS-VENDEDOR-ANT          PIC 9(02).
+           02 WS-TOT-VENDEDOR          PIC 9(07)V99.
+           02 WS-SUCURSAL-ANT          PIC 9(02).
+           02 WS-TOT-SUC               PIC 9(07)V99.
+           02 WS-TOT-EMPRESA           PIC 9(07)V99.
+           02 WS-TOTE-MA               PIC ZZ,ZZZ,ZZZ.ZZ.
+      ******************************************************************
+      *                       LINKAGE SECTION   
+      ****************************************************************** 
+       LINKAGE SECTION.
       ******************************************************************
       *                         PROCEDURE DIVISION   
       ******************************************************************                           
        PROCEDURE DIVISION.
            
-           PERFORM 10-INICIO
-              THRU 10-INICIO-F
+           PERFORM 100000-START
+              THRU 100000-START-F
            
-           PERFORM 20-PROCESO
-              THRU 20-PROCESO-F
-              UNTIL FS-STATUS-EOF           
+           PERFORM 200000-PROCESS
+              THRU 200000-PROCESS-F
+              UNTIL FS-STATUS-FILE-EOF           
            
-           PERFORM 30-FIN
-              THRU 30-FIN-F
+           PERFORM 300000-END
+              THRU 300000-END-F
            .
-            STOP RUN.
       ******************************************************************
-      *                         10-INICIO   
+      *                         100000-START   
       ******************************************************************      
-       10-INICIO.
+       100000-START.
            
-           PERFORM 10-ABRIR-DATOS1
-              THRU 10-ABRIR-DATOS1-F
+           PERFORM 110000-OPEN-DATOS1
+              THRU 110000-OPEN-DATOS1-F
            
-           PERFORM 20-LEER1
-              THRU 20-LEER1-F
+           PERFORM 210000-READ-DATOS1
+              THRU 210000-READ-DATOS1-F
            .
-       10-INICIO-F. EXIT.
+       100000-START-F. EXIT.
       ******************************************************************
-      *                         10-ABRIR-DATOS1   
+      *                         110000-OPEN-DATOS1   
       ******************************************************************     
-       10-ABRIR-DATOS1.
+       110000-OPEN-DATOS1.
            OPEN INPUT DATOS1
-           IF NOT FS-STATUS-OK
-               DISPLAY "ERROR AL ABRIR ARCHIVO " FS-STATUS1
+           IF NOT FS-STATUS-FILE-OK
+               DISPLAY "ERROR AL ABRIR ARCHIVO " FS-STATUS-FILE
            END-IF
            .
-       10-ABRIR-DATOS1-F. EXIT.
+       110000-OPEN-DATOS1-F. EXIT.
       ******************************************************************
-      *                         20-PROCESO   
+      *                         200000-PROCESS   
       ****************************************************************** 
-       20-PROCESO.  
+       200000-PROCESS.  
            MOVE REG-SUCURSAL TO WS-SUCURSAL-ANT
            INITIALIZE WS-TOT-SUC    
            
-           PERFORM 20-CORTE-SUCURSAL
-              THRU 20-CORTE-SUCURSAL-F
-              UNTIL WS-SUCURSAL-ANT <> REG-SUCURSAL
+           PERFORM UNTIL REG-SUCURSAL <> WS-SUCURSAL-ANT
+           
+               MOVE REG-VENDEDOR TO WS-VENDEDOR-ANT
+               INITIALIZE WS-TOT-VENDEDOR
+               
+               PERFORM UNTIL WS-VENDEDOR-ANT <> REG-VENDEDOR
+
+                   ADD REG-MONTO TO WS-TOT-VENDEDOR
+                   PERFORM 210000-READ-DATOS1
+                      THRU 210000-READ-DATOS1-F
+                      
+               END-PERFORM
+               
+               MOVE WS-TOT-VENDEDOR TO WS-TOTE-MA   
+               DISPLAY "TOTAL VENDEDOR " WS-VENDEDOR-ANT " : $" 
+                                                              WS-TOTE-MA   
+               COMPUTE WS-TOT-SUC = WS-TOT-SUC + WS-TOT-VENDEDOR
+           END-PERFORM  
 
            MOVE WS-TOT-SUC TO WS-TOTE-MA
-           ADD WS-TOT-SUC TO WS-TOT-EMPRESA   
+           ADD  WS-TOT-SUC TO WS-TOT-EMPRESA   
            DISPLAY "TOTAL " WS-SUCURSAL-ANT " : $" WS-TOTE-MA
            .         
-       20-PROCESO-F. EXIT.
+       200000-PROCESS-F. EXIT.
       ******************************************************************
-      *                         20-LEER1   
+      *                         210000-READ-DATOS1   
       ******************************************************************      
-       20-LEER1.
+       210000-READ-DATOS1.
            INITIALIZE REG-DATOS1
            READ DATOS1 INTO REG-DATOS1
            EVALUATE TRUE
-               WHEN FS-STATUS-OK
+               WHEN FS-STATUS-FILE-OK
                     CONTINUE
-               WHEN FS-STATUS-EOF
+               WHEN FS-STATUS-FILE-EOF
                     CONTINUE
            END-EVALUATE
            .
-       20-LEER1-F. EXIT.
+       210000-READ-DATOS1-F. EXIT.
       ******************************************************************
-      *                         20-CORTE-SUCURSAL   
-      ******************************************************************      
-       20-CORTE-SUCURSAL.
-           
-           MOVE REG-VENDEDOR TO WS-VENDEDOR-ANT
-           INITIALIZE WS-TOT-VENDEDOR
-
-           PERFORM 20-CORTE-VENDEDOR
-              THRU 20-CORTE-VENDEDOR-F
-              UNTIL WS-VENDEDOR-ANT <> REG-VENDEDOR
-           
-           MOVE WS-TOT-VENDEDOR TO WS-TOTE-MA   
-           DISPLAY "TOTAL VENDEDOR " WS-VENDEDOR-ANT " : $" WS-TOTE-MA   
-           COMPUTE WS-TOT-SUC = WS-TOT-SUC + WS-TOT-VENDEDOR
-           .
-       20-CORTE-SUCURSAL-F. EXIT.
-      ******************************************************************
-      *                         20-CORTE-VENDEDOR   
-      ******************************************************************      
-       20-CORTE-VENDEDOR.
-           ADD REG-MONTO TO WS-TOT-VENDEDOR
-           PERFORM 20-LEER1
-              THRU 20-LEER1-F
-           .
-       20-CORTE-VENDEDOR-F. EXIT.
-      ******************************************************************
-      *                         30-FIN   
+      *                         300000-END   
       ****************************************************************** 
-       30-FIN.
-           PERFORM 30-CERRAR-DATOS1
-              THRU 30-CERRAR-DATOS1-F
+       300000-END.
+           PERFORM 310000-CLOSE-DATOS1
+              THRU 310000-CLOSE-DATOS1-F
 
            PERFORM 30-VENTA-TOTAL
               THRU 30-VENTA-TOTAL-F
+           STOP RUN
            .    
-       30-FIN-F. EXIT.
+       300000-END-F. EXIT.
       ******************************************************************
-      *                         30-CERRAR-DATOS1   
+      *                         310000-CLOSE-DATOS1   
       ****************************************************************** 
-       30-CERRAR-DATOS1.
+       310000-CLOSE-DATOS1.
            CLOSE DATOS1
-           IF NOT FS-STATUS-OK
-               DISPLAY "ERROR AL CERRAR ARCHIVO " FS-STATUS1
+           IF NOT FS-STATUS-FILE-OK
+               DISPLAY "ERROR AL CERRAR ARCHIVO " FS-STATUS-FILE
            END-IF
            .
-       30-CERRAR-DATOS1-F. EXIT.
+       310000-CLOSE-DATOS1-F. EXIT.
       ******************************************************************
       *                         30-VENTA-TOTAL   
       ****************************************************************** 
