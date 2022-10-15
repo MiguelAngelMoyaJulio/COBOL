8f50dfe983fd49969f45bb5c819a080a5fe028e8
MiguelAngelMoyaJulio <mmoyajulio@frba.utn.edu.ar>
2022-10-15 20:35:30 -0300

    E29 CONTROL CUT - FLIGHTS REVIEW

--- EJERCICIOS/E-FILES/E29/E29.cbl
+++ EJERCICIOS/E-FILES/E29/E29.cbl
@@ -1,7 +1,6 @@
       ******************************************************************
-      * Author: MIGUEL MOYA
-      * Date: 2022-09-20
-      * Una compañía aérea desea emitir un listado con los movimientos mensuales de sus m vuelos al exterior.
+      * Una compañía aérea desea emitir un listado con los movimientos 
+      *mensuales de sus m vuelos al exterior.
       *ara ello cuenta con la siguiente información:
       *	De cada vuelo realizado el número de vuelo, destino, y 
       *antidad de asientos.
@@ -21,13 +20,27 @@
       ******************************************************************
        IDENTIFICATION DIVISION.
        PROGRAM-ID. E29.
+       AUTHOR. MIGUEL MOYA.
+       DATE-WRITTEN. OCTOBER 2022.
+       DATE-COMPILED. OCTOBER 2022.
+      ******************************************************************
+      *                     ENVIRONMENT DIVISION
+      ****************************************************************** 
        ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       SPECIAL-NAMES.
+             DECIMAL-POINT IS COMMA.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
-       SELECT OPTIONAL DATOS1
-       ASSIGN TO "VUELOS.txt"
-       FILE STATUS IS FS-STATUS1
-       ORGANIZATION IS LINE SEQUENTIAL.
+      ******************************************************************
+      *                            FILES   
+      ******************************************************************
+      *****************************  INPUT  ****************************
+       SELECT DATOS1 ASSIGN TO "VUELOS.txt"
+                     FILE STATUS IS FS-STATUS-FILE
+                     ORGANIZATION IS LINE SEQUENTIAL. 
+       
+      ****************************  OUTPUT  ****************************
        DATA DIVISION.
        FILE SECTION.
        FD DATOS1.
@@ -41,65 +54,75 @@
       *                     WORKING-STORAGE SECTION   
       ******************************************************************         
        WORKING-STORAGE SECTION.
-           01 FS-STATUS1                    PIC X(02) VALUE "00".
-               88 FS-STATUS-OK                        VALUE "00".
-               88 FS-STATUS-EOF                       VALUE "10".
-           01 WS-VAR.
-               02 WS-CANT-ASIENTO-OCUPADO     PIC 9(03).
-               02 WS-CANT-ASIENTO-DISPONIBLE  PIC 9(03).
-               02 WS-PORC-ASIENTO-LIBRE       PIC 9(03)V99.
-               02 WS-PORC-ASIENTO-OCUPADO  PIC 9(03)V99.
-               02 WS-DESTINO-ANT           PIC X(14).
-               02 WS-TOT-DESTINO           PIC 9(07)V99.
-               02 WS-TOT-EMPRESA           PIC 9(07)V99.
-               02 WS-TOTE-MA               PIC ZZ,ZZZ,ZZZ.ZZ.
-           01 WS-TITULO.
-               02 FILLER                   PIC X(03). 
-               02 T-PASAPORTE              PIC 9(06). 
-               02 FILLER                   PIC X(04). 
-               02 T-MONTO                  PIC  ZZ,ZZZ,ZZZ.ZZ.
+      ************************  CONSTANTS  *****************************
+
+      ************************** TABLES ********************************
+
+      **************************  SWITCHES  **************************** 
+       01 FS-STATUS-FILE                  PIC X(02) VALUE "00".
+           88 FS-STATUS-FILE-OK                     VALUE "00".
+           88 FS-STATUS-FILE-EOF                    VALUE "10".
+      
+      ************************** VARIABLES *****************************
+       01 WS-VAR.
+           02 WS-CANT-ASIENTO-OCUPADO     PIC 9(03).
+           02 WS-CANT-ASIENTO-DISPONIBLE  PIC 9(03).
+           02 WS-PORC-ASIENTO-LIBRE       PIC 9(03)V99.
+           02 WS-PORC-ASIENTO-OCUPADO     PIC 9(03)V99.
+           02 WS-DESTINO-ANT              PIC X(14).
+           02 WS-TOT-DESTINO              PIC 9(07)V99.
+           02 WS-TOT-EMPRESA              PIC 9(07)V99.
+           02 WS-TOTE-MA                  PIC ZZ.ZZZ.ZZZ,ZZ.
+       01 WS-TITULO.  
+           02 FILLER                      PIC X(03). 
+           02 T-PASAPORTE                 PIC 9(06). 
+           02 FILLER                      PIC X(04). 
+           02 T-MONTO                     PIC  ZZ.ZZZ.ZZZ,ZZ.
+      ******************************************************************
+      *                       LINKAGE SECTION   
+      ****************************************************************** 
+       LINKAGE SECTION.
       ******************************************************************
       *                         PROCEDURE DIVISION   
       ******************************************************************                           
        PROCEDURE DIVISION.
            
-           PERFORM 10-INICIO
-              THRU 10-INICIO-F
+           PERFORM 100000-START
+              THRU 100000-START-F
            
-           PERFORM 20-PROCESO
-              THRU 20-PROCESO-F
-              UNTIL FS-STATUS-EOF           
+           PERFORM 200000-PROCESS
+              THRU 200000-PROCESS-F
+              UNTIL FS-STATUS-FILE-EOF           
            
-           PERFORM 30-FIN
-              THRU 30-FIN-F
+           PERFORM 300000-END
+              THRU 300000-END-F
            .
-            STOP RUN.
       ******************************************************************
-      *                         10-INICIO   
+      *                         100000-START   
       ******************************************************************      
-       10-INICIO.
+       100000-START.
            
-           PERFORM 10-ABRIR-DATOS1
-              THRU 10-ABRIR-DATOS1-F
+           PERFORM 110000-OPEN-DATOS1
+              THRU 110000-OPEN-DATOS1-F
            
-           PERFORM 20-LEER1
-              THRU 20-LEER1-F
+           PERFORM 210000-READ-DATOS1
+              THRU 210000-READ-DATOS1-F
            .
-       10-INICIO-F. EXIT.
+       100000-START-F. EXIT.
       ******************************************************************
-      *                         10-ABRIR-DATOS1   
+      *                         110000-OPEN-DATOS1   
       ******************************************************************     
-       10-ABRIR-DATOS1.
+       110000-OPEN-DATOS1.
            OPEN INPUT DATOS1
-           IF NOT FS-STATUS-OK
-               DISPLAY "ERROR AL ABRIR ARCHIVO " FS-STATUS1
+           IF NOT FS-STATUS-FILE-OK
+               DISPLAY "ERROR AL ABRIR ARCHIVO " FS-STATUS-FILE
            END-IF
            .
-       10-ABRIR-DATOS1-F. EXIT.
+       110000-OPEN-DATOS1-F. EXIT.
       ******************************************************************
-      *                         20-PROCESO   
+      *                         200000-PROCESS   
       ****************************************************************** 
-       20-PROCESO.  
+       200000-PROCESS.  
            MOVE REG-DESTINO TO WS-DESTINO-ANT
            MOVE REG-CANTIDAD-ASIENTOS TO WS-CANT-ASIENTO-DISPONIBLE
            MOVE ZEROS TO WS-CANT-ASIENTO-OCUPADO
@@ -109,54 +132,46 @@
            
            DISPLAY "DESTINO " WS-DESTINO-ANT
            DISPLAY "NRO.PASAPORTE  IMPORTE(U$S) "
-           
-           PERFORM 20-CORTE-DESTINO
-              THRU 20-CORTE-DESTINO-F
-              UNTIL WS-DESTINO-ANT <> REG-DESTINO
+           PERFORM UNTIL WS-DESTINO-ANT <> REG-DESTINO
+               ADD 1 TO WS-CANT-ASIENTO-OCUPADO
+               COMPUTE WS-TOT-DESTINO = WS-TOT-DESTINO + REG-IMPORTE
+               MOVE REG-PASAPORTE TO T-PASAPORTE
+               MOVE REG-IMPORTE TO T-MONTO
+               DISPLAY WS-TITULO
+               PERFORM 210000-READ-DATOS1
+                  THRU 210000-READ-DATOS1-F
+           END-PERFORM
 
            COMPUTE WS-PORC-ASIENTO-OCUPADO = (WS-CANT-ASIENTO-OCUPADO * 
                    100) / WS-CANT-ASIENTO-DISPONIBLE 
            
-           COMPUTE WS-PORC-ASIENTO-LIBRE = 100.00 - 
+           COMPUTE WS-PORC-ASIENTO-LIBRE = 100,00 - 
                    WS-PORC-ASIENTO-OCUPADO
 
-           PERFORM 20-MOSTRAR-TOTAL-CORTE
-              THRU 20-MOSTRAR-TOTAL-CORTE-F        
+           PERFORM 220000-MOSTRAR-TOTAL-CORTE
+              THRU 220000-MOSTRAR-TOTAL-CORTE-F        
            .         
-       20-PROCESO-F. EXIT.
+       200000-PROCESS-F. EXIT.
       ******************************************************************
-      *                         20-LEER1   
+      *                         210000-READ-DATOS1   
       ******************************************************************      
-       20-LEER1.
+       210000-READ-DATOS1.
            INITIALIZE REG-DATOS1
            READ DATOS1 INTO REG-DATOS1
            EVALUATE TRUE
-               WHEN FS-STATUS-OK
+               WHEN FS-STATUS-FILE-OK
                     CONTINUE
-               WHEN FS-STATUS-EOF
+               WHEN FS-STATUS-FILE-EOF
                     CONTINUE
            END-EVALUATE
            .
-       20-LEER1-F. EXIT.
+       210000-READ-DATOS1-F. EXIT.
       ******************************************************************
-      *                         20-CORTE-DESTINO   
+      *                         220000-MOSTRAR-TOTAL-CORTE   
       ******************************************************************      
-       20-CORTE-DESTINO.
-           ADD 1 TO WS-CANT-ASIENTO-OCUPADO
-           COMPUTE WS-TOT-DESTINO = WS-TOT-DESTINO + REG-IMPORTE
-           MOVE REG-PASAPORTE TO T-PASAPORTE
-           MOVE REG-IMPORTE TO T-MONTO
-           DISPLAY WS-TITULO
-           PERFORM 20-LEER1
-              THRU 20-LEER1-F
-           .
-        20-CORTE-DESTINO-F. EXIT.
-      ******************************************************************
-      *                         20-MOSTRAR-TOTAL-CORTE   
-      ******************************************************************      
-       20-MOSTRAR-TOTAL-CORTE.
+       220000-MOSTRAR-TOTAL-CORTE.
            MOVE WS-TOT-DESTINO TO WS-TOTE-MA
-           ADD WS-TOT-DESTINO TO WS-TOT-EMPRESA   
+           ADD  WS-TOT-DESTINO TO WS-TOT-EMPRESA   
            DISPLAY "TOTAL RECAUDADO DEL VUELO " WS-DESTINO-ANT 
                    " : $" WS-TOTE-MA
            DISPLAY "% DE ASIENTOS LIBRES DEL VUELO : "
@@ -165,34 +180,35 @@
            DISPLAY "% DE ASIENTOS OCUPADOS DEL VUELO : "
                    WS-PORC-ASIENTO-OCUPADO
            .
-        20-MOSTRAR-TOTAL-CORTE-F. EXIT.
+        220000-MOSTRAR-TOTAL-CORTE-F. EXIT.
       ******************************************************************
-      *                         30-FIN   
+      *                         300000-END   
       ****************************************************************** 
-       30-FIN.
-           PERFORM 30-CERRAR-DATOS1
-              THRU 30-CERRAR-DATOS1-F
+       300000-END.
+           PERFORM 310000-CLOSE-DATOS1
+              THRU 310000-CLOSE-DATOS1-F
 
-           PERFORM 30-VENTA-TOTAL
-              THRU 30-VENTA-TOTAL-F
+           PERFORM 320000-VENTA-TOTAL
+              THRU 320000-VENTA-TOTAL-F
+           STOP RUN
            .    
-       30-FIN-F. EXIT.
+       300000-END-F. EXIT.
       ******************************************************************
-      *                         30-CERRAR-DATOS1   
+      *                         310000-CLOSE-DATOS1   
       ****************************************************************** 
-       30-CERRAR-DATOS1.
+       310000-CLOSE-DATOS1.
            CLOSE DATOS1
-           IF NOT FS-STATUS-OK
-               DISPLAY "ERROR AL CERRAR ARCHIVO " FS-STATUS1
+           IF NOT FS-STATUS-FILE-OK
+               DISPLAY "ERROR AL CERRAR ARCHIVO " FS-STATUS-FILE
            END-IF
            .
-       30-CERRAR-DATOS1-F. EXIT.
+       310000-CLOSE-DATOS1-F. EXIT.
       ******************************************************************
-      *                         30-VENTA-TOTAL   
+      *                         320000-VENTA-TOTAL   
       ****************************************************************** 
-       30-VENTA-TOTAL.
+       320000-VENTA-TOTAL.
            MOVE WS-TOT-EMPRESA TO WS-TOTE-MA   
            DISPLAY "TOTAL RECAUDADO EN EL MES $" WS-TOTE-MA 
            .
-       30-VENTA-TOTAL-F. EXIT.
+       320000-VENTA-TOTAL-F. EXIT.
        END PROGRAM E29.
