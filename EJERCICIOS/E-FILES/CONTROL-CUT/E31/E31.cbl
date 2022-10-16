135df6dd8a2d944af8ecfd10a8aa5753aa7772de
MiguelAngelMoyaJulio <mmoyajulio@frba.utn.edu.ar>
2022-10-15 23:11:19 -0300

    E31 PRESIDENTIAL ELECTION

--- EJERCICIOS/E-FILES/CONTROL-CUT/E31/E31.cbl
+++ EJERCICIOS/E-FILES/CONTROL-CUT/E31/E31.cbl
@@ -6,15 +6,28 @@
        PROGRAM-ID. E31.
        AUTHOR. MIGUEL MOYA.
        DATE-WRITTEN. SEPTEMBER 2022.
+       AUTHOR. MIGUEL MOYA.
+       DATE-WRITTEN. OCTOBER 2022.
+       DATE-COMPILED. OCTOBER 2022.
+      ******************************************************************
+      *                     ENVIRONMENT DIVISION
+      ****************************************************************** 
        ENVIRONMENT DIVISION.
        CONFIGURATION SECTION.
        SPECIAL-NAMES.
-       DECIMAL-POINT IS COMMA.
+             DECIMAL-POINT IS COMMA.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
+      ******************************************************************
+      *                            FILES   
+      ******************************************************************
+      *****************************  INPUT  ****************************
        SELECT DATOS1 ASSIGN TO "LOTE.txt"
-                     FILE STATUS IS FS-STATUS1
-       ORGANIZATION IS LINE SEQUENTIAL.
+                     FILE STATUS IS FS-STATUS-FILE
+                     ORGANIZATION IS LINE SEQUENTIAL. 
+       
+      ****************************  OUTPUT  ****************************       
+
        DATA DIVISION.
        FILE SECTION.
        FD DATOS1.
@@ -26,109 +39,101 @@
       *                     WORKING-STORAGE SECTION   
       ******************************************************************
        WORKING-STORAGE SECTION.
-          01 FS-STATUS1                    PIC X(02) VALUE "00".
-             88 FS-STATUS-OK                        VALUE "00".
-             88 FS-STATUS-EOF                       VALUE "10".
-          01 WS-VAR.
-             02 WS-CANDIDATE-ANT           PIC 9(02).
-          01 WS-TITULO.
-               02 FILLER                   PIC X(03). 
-               02 T-VENDEDOR               PIC 9(02). 
-               02 FILLER                   PIC X(10). 
-               02 T-FACTURA                PIC 9(02). 
-               02 FILLER                   PIC X(04). 
-               02 T-MONTO                  PIC  ZZ.ZZZ.ZZZ,ZZ.
+      ************************  CONSTANTS  *****************************
+
+      ************************** TABLES ********************************
+
+      **************************  SWITCHES  **************************** 
+       01 FS-STATUS-FILE                    PIC X(02) VALUE "00".
+          88 FS-STATUS-FILE-OK                        VALUE "00".
+          88 FS-STATUS-FILE-EOF                       VALUE "10".
+
+      ************************** VARIABLES *****************************
+       01 WS-VAR.
+          02 WS-CANDIDATE-ANT           PIC 9(02).
       ******************************************************************
       *                         PROCEDURE DIVISION   
       ******************************************************************
        PROCEDURE DIVISION.
-                                                                          
-           PERFORM 10-INICIO                      
-              THRU 10-INICIO-F                    
+           PERFORM 100000-START                      
+              THRU 100000-START-F                    
+
+           PERFORM 200000-PROCESS                     
+              THRU 200000-PROCESS-F                   
+              UNTIL FS-STATUS-FILE-EOF                 
                                                   
-           PERFORM 20-PROCESO                     
-              THRU 20-PROCESO-F                   
-              UNTIL FS-STATUS-EOF                 
-                                                  
-           PERFORM 30-FIN                         
-              THRU 30-FIN-F                       
-           .                                      
-            STOP RUN.                             
+           PERFORM 300000-END                         
+              THRU 300000-END-F                       
+           .
       ******************************************************************
-      *                         10-INICIO         
+      *                         100000-START         
       ******************************************************************
-       10-INICIO.                                 
-           PERFORM 10-ABRIR-DATOS1                
-              THRU 10-ABRIR-DATOS1-F
+       100000-START.                                 
+           PERFORM 110000-OPEN-DATOS1                
+              THRU 110000-OPEN-DATOS1-F
                             
-           PERFORM 20-LEER1                       
-              THRU 20-LEER1-F                     
+           PERFORM 210000-READ-DATOS1                       
+              THRU 210000-READ-DATOS1-F                     
            .                                      
-       10-INICIO-F. EXIT.                         
+       100000-START-F. EXIT.                         
       ******************************************************************
-      *                         10-ABRIR-DATOS1   
+      *                         110000-OPEN-DATOS1   
       ******************************************************************
-       10-ABRIR-DATOS1.                        
+       110000-OPEN-DATOS1.                        
            OPEN INPUT DATOS1                   
-           IF NOT FS-STATUS-OK
-               DISPLAY "ERROR AL ABRIR ARCHIVO " FS-STATUS1
+           IF NOT FS-STATUS-FILE-OK
+               DISPLAY "ERROR AL ABRIR ARCHIVO " FS-STATUS-FILE
            END-IF
            .
-       10-ABRIR-DATOS1-F. EXIT.
+       110000-OPEN-DATOS1-F. EXIT.
       ******************************************************************
-      *                         20-PROCESO   
+      *                         200000-PROCESS   
       ****************************************************************** 
-       20-PROCESO.
+       200000-PROCESS.
              
            MOVE REG-CANDIDATE TO WS-CANDIDATE-ANT
            
            DISPLAY "CANDIDATE : " WS-CANDIDATE-ANT
            DISPLAY "PROVINCE" "                                   " 
                    "VOTES" 
-           PERFORM 20-CORTE-CANDIDATE 
-              THRU 20-CORTE-CANDIDATE-F
-              UNTIL WS-CANDIDATE-ANT <> REG-CANDIDATE
+           PERFORM UNTIL WS-CANDIDATE-ANT <> REG-CANDIDATE
+             DISPLAY REG-PROVINCE "     " REG-VOTES 
+             PERFORM 210000-READ-DATOS1
+                THRU 210000-READ-DATOS1-F
+           END-PERFORM
            .         
-       20-PROCESO-F. EXIT.
+       200000-PROCESS-F. EXIT.
       ******************************************************************
-      *                         20-LEER1   
+      *                         210000-READ-DATOS1   
       ******************************************************************      
-       20-LEER1.
+       210000-READ-DATOS1.
            INITIALIZE REG-DATOS1
            READ DATOS1 INTO REG-DATOS1
            EVALUATE TRUE
-               WHEN FS-STATUS-OK
-                    CONTINUE
-               WHEN FS-STATUS-EOF
-                    CONTINUE
+             WHEN FS-STATUS-FILE-OK
+                  CONTINUE
+             WHEN FS-STATUS-FILE-EOF
+                  CONTINUE
            END-EVALUATE
            .
-       20-LEER1-F. EXIT.
+       210000-READ-DATOS1-F. EXIT.
       ******************************************************************
-      *                         20-CORTE-CANDIDATE   
-      ******************************************************************      
-       20-CORTE-CANDIDATE.
-           DISPLAY REG-PROVINCE "     " REG-VOTES 
-           PERFORM 20-LEER1
-              THRU 20-LEER1-F
-           .
-       20-CORTE-CANDIDATE-F. EXIT.
-      ******************************************************************
-      *                         30-FIN   
+      *                         300000-END   
       ****************************************************************** 
-       30-FIN.
-           PERFORM 30-CERRAR-DATOS1
-              THRU 30-CERRAR-DATOS1-F
+       300000-END.
+           PERFORM 310000-CLOSE-DATOS1
+              THRU 310000-CLOSE-DATOS1-F
+           STOP RUN   
            .    
-       30-FIN-F. EXIT.
+       300000-END-F. EXIT.
       ******************************************************************
-      *                         30-CERRAR-DATOS1   
+      *                         310000-CLOSE-DATOS1   
       ****************************************************************** 
-       30-CERRAR-DATOS1.
+       310000-CLOSE-DATOS1.
            CLOSE DATOS1
-           IF NOT FS-STATUS-OK
-               DISPLAY "ERROR AL CERRAR ARCHIVO " FS-STATUS1
+           IF NOT FS-STATUS-FILE-OK
+               DISPLAY "ERROR AL CERRAR ARCHIVO " FS-STATUS-FILE
            END-IF
            .
-       30-CERRAR-DATOS1-F. EXIT.
+       310000-CLOSE-DATOS1-F. EXIT.
        END PROGRAM E31.
